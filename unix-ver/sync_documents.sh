5dd50622006784384a8fa3f365976fd0ecd12fe7
Tobias <tobias.janssn@gmail.com>
2026-01-15 00:30:00 +0100

    feat: Add a bit of documentation

--- unix-ver/sync_documents.sh
+++ unix-ver/sync_documents.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Creates variables from env.sh
 source env.sh
 
-if [ "$( tail -n 1 "$LOCAL_CONFIG" )" = "" ];
-then
-    echo "" >> "$LOCAL_CONFIG"
-fi
+# Adds empty newline to the end of sync_ignore if none is present
+# To make sure --exclude-from flag reads all lines properly
+if [ "$( tail -n 1 "$LOCAL_CONFIG" )" = "" ]; then echo "" >> "$LOCAL_CONFIG"; fi
 
+# Push and pull to remote directory and prints output to logs
+# Ignores any fiels specified in "sync_ignore"
 {
     echo "Sync started at ${NOW} on ${DEVICE} to ${REMOTE_USER}@${REMOTE}"
     rsync --archive --compress --partial --verbose --exclude-from="$LOCAL_CONFIG" "${LOCAL_DIR}/" "${REMOTE_USER}@${REMOTE}:/backup/${REMOTE_DIR}"
