d3e1af6ad931af62d1b9162bdebb0b5e1d18edcc
Tobias <tobias.janssn@gmail.com>
2026-01-14 23:30:37 +0100

    feat: Better logging and add envfile

---
+++ unix-ver/env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REMOTE_DIR=test_dir
+LOCAL_DIR=./test_dest
+REMOTE_USER=pi
+REMOTE="<IP>"
+LOCAL_CONFIG=sync_ignore
+LOGFILE=./log
+DEVICE=$(hostname)
+NOW=$(date)
--- unix-ver/sync_documents.sh
+++ unix-ver/sync_documents.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-REMOTE_DIR=test_dir
-LOCAL_DIR=./test_dest
-REMOTE_USER=pi
-REMOTE="<IP>"
-LOCAL_CONFIG=sync_ignore
-LOGFILE=./log
-DEVICE=$(hostname)
-NOW=$(date)
+source env.sh
 
-if [ "$( tail -n 1 $LOCAL_CONFIG )"="" ];
+if [ "$( tail -n 1 "$LOCAL_CONFIG" )" = "" ];
 then
-    echo "" >> $LOCAL_CONFIG
+    echo "" >> "$LOCAL_CONFIG"
 fi
 
-echo "Sync started at ${NOW} on ${DEVICE} to ${REMOTE_USER}@${REMOTE}" >> $LOGFILE
-rsync --archive --compress --partial --verbose --exclude-from=$LOCAL_CONFIG "${LOCAL_DIR}/" "${REMOTE_USER}@${REMOTE}:/backup/${REMOTE_DIR}" >> $LOGFILE
-rsync --archive --compress --partial --verbose --exclude-from=$LOCAL_CONFIG "${REMOTE_USER}@${REMOTE}:/backup/${REMOTE_DIR}/" "${LOCAL_DIR}" >> $LOGFILE
-printf -- "-%.0s" {1..100} >> $LOGFILE
-printf '\n' >> $LOGFILE
+{
+    echo "Sync started at ${NOW} on ${DEVICE} to ${REMOTE_USER}@${REMOTE}"
+    rsync --archive --compress --partial --verbose --exclude-from="$LOCAL_CONFIG" "${LOCAL_DIR}/" "${REMOTE_USER}@${REMOTE}:/backup/${REMOTE_DIR}"
+    rsync --archive --compress --partial --verbose --exclude-from="$LOCAL_CONFIG" "${REMOTE_USER}@${REMOTE}:/backup/${REMOTE_DIR}/" "${LOCAL_DIR}"
+    printf -- "-%.0s" {1..100}
+    printf '\n'
+} >> "$LOGFILE"
